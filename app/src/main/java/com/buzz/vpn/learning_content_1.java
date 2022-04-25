d4b4d33720455019768759c16512dd837809fa23
Jslifkin101 <Jslifkin101@gmail.com>
2022-04-25 01:14:39 -0400

    Added in the 2 articles for PDF and added Quizzes

---
+++ app/src/main/java/com/buzz/vpn/learning_content_1.java
@@ -0,0 +1,51 @@
+package com.buzz.vpn;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class learning_content_1 extends Activity {
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
+    }
+
+    PDFView Pdfview;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.learning_content);
+
+
+
+        Pdfview = (PDFView) findViewById(R.id.pdfview);
+
+        Pdfview.fromAsset("Lesson_1.pdf").load();
+
+        RelativeLayout scrollable = findViewById(R.id.scrollable);
+
+
+        scrollable.setBackgroundColor(getResources().getColor(R.color.colorLightBackground));
+
+
+    }
+}
---
+++ app/src/main/java/com/buzz/vpn/practice_questions.java
@@ -0,0 +1,6 @@
+package com.buzz.vpn;
+
+import android.app.Activity;
+
+public class practice_questions extends Activity {
+}
