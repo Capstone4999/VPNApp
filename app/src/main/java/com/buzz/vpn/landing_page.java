af43ecec3c0556f9ea05fb65aa015182f83d782e
jonahragle <65098205+jonahragle@users.noreply.github.com>
2022-04-25 22:26:43 -0400

    jonah14

--- app/src/main/java/com/buzz/vpn/landing_page.java
+++ app/src/main/java/com/buzz/vpn/landing_page.java
@@ -1,14 +1,25 @@
 package com.buzz.vpn;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class landing_page extends AppCompatActivity {
+    Button nextPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+        nextPage = (Button) findViewById(R.id.signup_button);
+
+        nextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), userLogin_activity.class));
+            }
+        });
     }
 }
